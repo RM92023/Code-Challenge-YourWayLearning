eaac2fc795df9b0581b436fbe486def4a239bf23
RM92023 <robinjara20@gmail.com>
2025-07-18 00:13:09 -0500

    feat(HU04): calculate and display WPM only after user starts typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import TypingInput from "./components/TypingInput";
 import { useTypingTest } from "./hooks/useTypingTest";
-import "./App.css";
 import ControlButtons from "./components/ControlButtons";
+import "./App.css";
 
 const App = () => {
-  const { input, onInputChange, words, isFinished, isStarted, reset } = useTypingTest("This is the sentence to type");
+  const {
+    input,
+    onInputChange,
+    words,
+    isFinished,
+    isStarted,
+    reset,
+    wpm,
+  } = useTypingTest("This is the sentence to type");
 
   return (
     <main className="App">
       <h1>Typing Speed Test</h1>
-      <h3>{isFinished ? "Test Completed!" : "Type the following:"}</h3>
 
-      <p>
-        {words.join(" ") + " "}
-      </p>
+      <h3>
+        {isFinished
+          ? `Test Completed! WPM: ${wpm}`
+          : "Type the following:"}
+      </h3>
+
+      <p>{words.join(" ")}</p>
 
       <TypingInput
         input={input}
         onChange={onInputChange}
         isFinished={isFinished}
       />
-      <ControlButtons onReset={reset} isFinished={isFinished} isStarted={isStarted} />
+
+      <ControlButtons
+        onReset={reset}
+        isFinished={isFinished}
+        isStarted={isStarted}
+      />
     </main>
   );
 };
