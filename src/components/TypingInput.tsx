5b423462d334ea7c12254d02d4dc20e343f031cb
RM92023 <robinjara20@gmail.com>
2025-07-17 23:05:56 -0500

    feat(HU01): hide input on test completion

---
+++ src/components/TypingInput.tsx
@@ -0,0 +1,22 @@
+interface Props {
+  input: string;
+  onChange: (value: string) => void;
+  isFinished: boolean;
+}
+
+const TypingInput = ({ input, onChange, isFinished }: Props) => {
+  if (isFinished) return null;
+
+  return (
+    <input
+      name="text"
+      value={input}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder="Start typing..."
+      autoFocus
+      className="typing-input"
+    />
+  );
+};
+
+export default TypingInput;
